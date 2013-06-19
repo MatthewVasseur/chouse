7c0d071f0485f328980bfabba718f81a0393a3dc
Matthew Vasseur <matt@orchive.com>
2013-06-19 13:11:24 -0400

    strong start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chouse::Application.config.secret_key_base = '5261ce9737f9232251133674df753730ff072ea949474f89303740fdbe52eae1d37d54a592b631e392c78b040cda66033693d9141d076ea9b6cc13a7819dc8b5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Chouse::Application.config.session_store :cookie_store, key: '_chouse_session'
